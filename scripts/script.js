a8d977fc2a05796f5aeadaddba0c6d0cc9e0cea1
Johanne McClanahan <jmcclenahan@ewu.edu>
2023-05-08 22:18:59 -0700

    added new playlist demo and linked login button to playlist demo for showcase

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,16 @@
 
+$("#press").click(function(){
 
-alert("hello World");
+    if($(this).next().css('display') === "none"){
+    $(this).next().slideDown(300);
+    $(this).next().css("border-bottom-color", "silver");
+
+    }
+    else{
+        
+        $(this).next().slideUp(300);
+
+    }
+   
+});
+

83310de964bf98846c9efbb00596e46dc75f0a85
JohanneJayde <jmcclenahan@ewu.edu>
2023-05-15 14:26:48 -0700

    Autorization works but only on the "click link" button on the userview page. Clicking the link will redirect to the authorization link

---
+++ scripts/routing.js
@@ -0,0 +1,22 @@
+
+
+$("#click").on("click", async function(){
+    var link = await getAuthLink();
+	window.location.href = link;
+
+});
+
+
+async function  getAuthLink(){
+    
+	const response = await fetch("https://spotlist.patchyserver.xyz/api/auth", {
+	method: 'GET',
+	headers: {
+		'Accept': 'application/json',
+  		'Content-Type': 'application/json'
+	}
+	}).catch((error)=>{
+		console.error('Error:',error);
+	})
+	return response.json();
+}

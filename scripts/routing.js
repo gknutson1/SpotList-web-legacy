c6255b919484cf48bef44655bfbe5d643efb7df8
JohanneJayde <jmcclenahan@ewu.edu>
2023-05-19 17:38:16 -0700

    added way to get state and code back from spotify authorization website

--- scripts/routing.js
+++ scripts/routing.js
@@ -7,7 +7,7 @@ $("#click").on("click", async function(){
 });
 
 
-//get code and state from callack url
+//get code and state from callack url DONE
 //send them to the /exchange/
 //recieves token and user name
 //get the actual playlist using get temp playlist
@@ -25,4 +25,23 @@ async function getAuthLink(){
 		console.error('Error:',error);
 	})
 	return response.json();
+}
+
+var code;
+var state;
+
+
+/*
+getCodeAndState() return the user's code and state for usage in /exchange route. THis is is only
+called within the callback.html will is just an intermediate URL
+*/
+function getCodeAndState(){
+
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+
+	code = urlParams.get('code');
+	state = urlParams.get('state');
+	window.location.href = "/index";
+
 }

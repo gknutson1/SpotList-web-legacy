f40b7ec91d832893dffb7429790950c6734bae6c
JohanneJayde <jmcclenahan@ewu.edu>
2023-05-26 20:43:03 -0700

    added new routing for getting user_id, token, and display_name

--- scripts/routing.js
+++ scripts/routing.js
@@ -1,12 +1,4 @@
 
-
-$("#click").on("click", async function(){
-    var link = await getAuthLink();
-	window.location.href = link;
-
-});
-
-
 //get code and state from callack url DONE
 //send them to the /exchange/
 //recieves token and user name
@@ -29,19 +21,65 @@ async function getAuthLink(){
 
 var code;
 var state;
-
+var urlParams;
 
 /*
 getCodeAndState() return the user's code and state for usage in /exchange route. THis is is only
 called within the callback.html will is just an intermediate URL
 */
-function getCodeAndState(){
+async function getCodeAndState(){
 
 	const queryString = window.location.search;
-	const urlParams = new URLSearchParams(queryString);
+	urlParams = new URLSearchParams(queryString);
 
 	code = urlParams.get('code');
 	state = urlParams.get('state');
+
+	setIDandToken();
+	//setPlaylistInfo();
 	window.location.href = "/index";
 
 }
+var newURL;
+async function getTokenandID(){
+	newURL = "https://spotlist.patchyserver.xyz/api/exchange?code=" + code + "&state=" + state;
+	const response = await fetch(newURL,{
+		method: 'GET',
+		headers: {
+			'Content-type': 'application/json',
+			'Accept': 'application/json'
+		}
+	}).catch((error)=>{
+		console.error('Error:',error);
+	})
+	return response.json();
+}
+
+
+var user_id;
+var token;
+var username;
+
+async function setIDandToken(){
+	const exchange = await getTokenandID();
+	user_id = exchange.user_id;
+	token = exchange.token;
+	username = exchange.display_name;
+	alert("Hello " + username + "!" );
+}
+
+async function getPlaylistInfo(){
+	const response = await fetch("https://spotlist.patchyserver.xyz/api/temp/playlists?user_id=" + user_id + "&token=" + token,{
+		method: 'GET',
+		headers: {
+			'Content-type': 'application/json',
+			'Accept': 'application/json'
+		}
+	}).catch((error)=>{
+		console.error('Error:',error);
+	})
+	console.log(response.json())
+	return response.json();
+}
+
+
--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,26 @@
 
-$("#press").click(function(){
 
-    if($(this).next().css('display') === "none"){
-    $(this).next().slideDown(300);
-    $(this).next().css("border-bottom-color", "silver");
+document.getElementById("connect").addEventListener("click",async function(){
+    var link = await getAuthLink();
+	window.location.href = link;
 
-    }
-    else{
+} );
+
+
+// $("#press").click(function(){
+
+//     if($(this).next().css('display') === "none"){
+//     $(this).next().slideDown(300);
+//     $(this).next().css("border-bottom-color", "silver");
+
+//     }
+//     else{
         
-        $(this).next().slideUp(300);
+//         $(this).next().slideUp(300);
 
-    }
+//     }
    
-});
+// });
 
 document.getElementById("home-button").addEventListener("click", function(){
     window.location = "index.html";

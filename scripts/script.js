689d506c5ebd9ab5d6ad391c338b886472899fd0
JohanneJayde <jmcclenahan@ewu.edu>
2023-06-03 11:59:28 -0700

    added ability for search input to be changed based on your rule. i.e filter by date will use data picker

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,36 @@
 document.getElementById("home-button").addEventListener("click", function(){
     window.location = "/index";
 });
+
+
+document.getElementById("add-rule-btn").addEventListener("click", function(){
+    addRule();
+})
+
+document.getElementById("rule-one").childNodes[1].elements[1].addEventListener("change", function(e){
+    alert(e.target.value);
+
+
+})
+
+function addRule(){
+    const ruleTemp = document.getElementById("rule-one");
+
+    const copy = ruleTemp.cloneNode("true");
+
+    copy.childNodes[1].elements[1].addEventListener("change", function(e){
+        if(e.target.value.match(/^released./)){
+            copy.childNodes[1].elements[2].type ="date";
+        }
+        else{
+            copy.childNodes[1].elements[2].type ="text";
+        }
+    })
+
+
+    document.getElementById("rules-list").appendChild(copy);
+}
+
+function generateRule(){
+    
+}

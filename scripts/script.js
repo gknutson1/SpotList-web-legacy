3e027eb9c353562e515f9bae38142ffbfa8119c2
JohanneJayde <jmcclenahan@ewu.edu>
2023-06-04 11:45:38 -0700

    added safeguard for saving rules and editing them so it's easier to keep track of them before we apply them

--- scripts/script.js
+++ scripts/script.js
@@ -2,26 +2,11 @@ document.getElementById("home-button").addEventListener("click", function(){
     window.location = "/index";
 });
 
-// function checkRuleOneSave(){
-//     const formElements = document.getElementById("rule-one").childNodes[1].elements;
-//     const searchField = formElements[2].value;
 
-//     if(searchField === null || searchField.trim() === ""){
-//         alert("Please enter in search for first rule");
-//     }
-//     else{
-//         document.getElementById("add-rule-btn").removeEventListener("click",checkRuleOneSave,false);
-//         addRule();
-//         document.getElementById("add-rule-btn").addEventListener("click", function(){
-//             addRule();
-//         });
-//     }
-
-// }
 document.getElementById("add-rule-btn").addEventListener("click", addRule);
 //document.getElementById("add-rule-btn").addEventListener("click", checkRuleOneSave);
 
-applyChangeListener(document.getElementById("rule-one").childNodes[1].elements);
+applyListeners(document.getElementById("rule-one").childNodes[1].elements);
 
 function addRule(){
     const ruleTemp = document.getElementById("rule-one");
@@ -32,7 +17,7 @@ function addRule(){
     document.getElementById("rules-list").appendChild(newRule);
 }
 
-function applyChangeListener(ruleForm){
+function applyListeners(ruleForm){
     ruleForm[1].addEventListener("change", function(){
         if(ruleForm[1].value.match(/^released./)){
             ruleForm[2].type ="date";
@@ -43,6 +28,22 @@ function applyChangeListener(ruleForm){
             ruleForm[2].value = "";
 
         }
+    });
+    ruleForm[3].addEventListener("click", function(){
+        ruleForm[0].disabled = true;
+        ruleForm[1].disabled = true;
+        ruleForm[2].disabled = true;
+        ruleForm[3].disabled = true;
+        ruleForm[4].disabled = false;
+
+    });
+    ruleForm[4].addEventListener("click", function(){
+        ruleForm[0].disabled = false;
+        ruleForm[1].disabled = false;
+        ruleForm[2].disabled = false;
+        ruleForm[3].disabled = false;
+        ruleForm[4].disabled = true;
+
     });
 }
 
@@ -56,36 +57,10 @@ function cleanClone(newRule){
     const ruleForm = newRule.childNodes[1].elements;
 
     ruleForm[0].options[1].disabled = false;
-    applyChangeListener(ruleForm);
+    applyListeners(ruleForm);
     ruleForm[1].options[1].selected = true;
     ruleForm[2].type ="text";
     ruleForm[2].value = "";
-
+    ruleForm[5].disabled = false;
     return newRule;
 }
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.addEventListener("click", function(){
-  modal.style.display = "block";
-});
-
-// When the user clicks on <span> (x), close the modal
-span.addEventListener("click", function() {
-  modal.style.display = "none";
-});
-
-// When the user clicks anywhere outside of the modal, close it
-window.addEventListener("click", function(e){
-  if(e.target == modal) {
-    modal.style.display = "none";
-  }
-});

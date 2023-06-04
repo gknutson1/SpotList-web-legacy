e8c5ebb7f7d1b76dc25803af8974265b2dc1b772
JohanneJayde <jmcclenahan@ewu.edu>
2023-06-04 12:24:28 -0700

    color coding based on the operation you want to do for a rule (save or delete) is now possible

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-document.getElementById("home-button").addEventListener("click", function(){
+document.getElementById("home-button").addEventListener("click", function () {
     window.location = "/index";
 });
 
@@ -6,68 +6,78 @@ document.getElementById("home-button").addEventListener("click", function(){
 document.getElementById("add-rule-btn").addEventListener("click", addRule);
 //document.getElementById("add-rule-btn").addEventListener("click", checkRuleOneSave);
 
-applyListeners(document.getElementById("rule-one").childNodes[1].elements);
-function addRule(){
+applyListeners(document.getElementById("rule-one"));
+
+function addRule() {
     const ruleTemp = document.getElementById("rule-one");
 
     let newRule = ruleTemp.cloneNode("true");
     newRule = cleanClone(newRule);
 
-    document.getElementById("rules-list").appendChild(newRule);
+    document.getElementById("rules-list").insertBefore(newRule, document.getElementById("add-rule-btn"));
+    
 }
 
-function applyListeners(ruleForm){
-    ruleForm[1].addEventListener("change", function(){
-        if(ruleForm[1].value.match(/^released./)){
-            ruleForm[2].type ="date";
+function applyListeners(rule) {
+
+    const ruleForm = rule.childNodes[1].elements;
+
+    ruleForm[1].addEventListener("change", function () {
+        if (ruleForm[1].value.match(/^released./)) {
+            ruleForm[2].type = "date";
             ruleForm[2].value = "2016-10-13";
         }
-        else{
-            ruleForm[2].type ="text";
+        else {
+            ruleForm[2].type = "text";
             ruleForm[2].value = "";
 
         }
     });
-    ruleForm[3].addEventListener("click", function(){
+    ruleForm[3].addEventListener("click", function () {
         ruleForm[0].disabled = true;
         ruleForm[1].disabled = true;
         ruleForm[2].disabled = true;
         ruleForm[3].disabled = true;
         ruleForm[4].disabled = false;
+        rule.childNodes[1].style.borderColor = "green";
+
 
     });
-    ruleForm[4].addEventListener("click", function(){
+    ruleForm[4].addEventListener("click", function () {
         ruleForm[0].disabled = false;
         ruleForm[1].disabled = false;
         ruleForm[2].disabled = false;
         ruleForm[3].disabled = false;
         ruleForm[4].disabled = true;
+        rule.childNodes[1].style.borderColor = "blue";
+
 
     });
 
 }
 
-function generateRule(){
-    
+function generateRule() {
+
 }
 /*
 CleanClone() will clean clone of orignal first rule.
 */
-function cleanClone(newRule){
+function cleanClone(newRule) {
     const ruleForm = newRule.childNodes[1].elements;
     applyDeleteFilter(newRule);
     ruleForm[0].options[1].disabled = false;
-    applyListeners(ruleForm);
+    applyListeners(newRule);
     ruleForm[1].options[1].selected = true;
-    ruleForm[2].type ="text";
+    ruleForm[2].type = "text";
     ruleForm[2].value = "";
     ruleForm[5].disabled = false;
+    newRule.childNodes[1].style.borderColor = "black";
 
     return newRule;
 }
 
-function applyDeleteFilter(rule){
-    rule.childNodes[1].elements[5].addEventListener("click",function(){
+function applyDeleteFilter(rule) {
+    rule.childNodes[1].elements[5].addEventListener("click", function () {
         rule.remove();
     })
 }
